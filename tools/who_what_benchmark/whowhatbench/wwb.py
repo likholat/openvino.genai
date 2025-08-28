669f7dabfe85468964d0a1e1ba52bd0ce314a432
likholat <anna.likholat@intel.com>
2025-08-28 18:22:21 +0200

    fix

--- tools/who_what_benchmark/whowhatbench/wwb.py
+++ tools/who_what_benchmark/whowhatbench/wwb.py
@@ -322,8 +322,13 @@ def diff_strings(a: str, b: str, *, use_loguru_colors: bool = False) -> str:
     return "".join(output)
 
 
-def genai_gen_text(model, tokenizer, question, max_new_tokens, skip_question, use_chat_template=False):
-    return model.generate(question, do_sample=False, max_new_tokens=max_new_tokens, apply_chat_template=use_chat_template)
+def genai_gen_text(model, tokenizer, question, max_new_tokens, skip_question, use_chat_template=False, empty_adapters=False):
+    kwargs = {}
+    if empty_adapters:
+        import openvino_genai
+        kwargs["adapters"] = openvino_genai.AdapterConfig()
+
+    return model.generate(question, do_sample=False, max_new_tokens=max_new_tokens, apply_chat_template=use_chat_template, **kwargs)
 
 
 def llamacpp_gen_text(model, tokenizer, question, max_new_tokens, skip_question, use_chat_template=False):
